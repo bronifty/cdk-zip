991e01b5d4e0fc99c53a31a2d878e3a4dca48961
bronifty <bronifty@gmail.com>
2024-05-26 19:23:45 -0400

    this

--- lib/cdk-zip-stack.ts
+++ lib/cdk-zip-stack.ts
@@ -1,16 +1,18 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkZipStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkZipQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.aws_lambda.Function(this, "fsreadfile", {
+      runtime: cdk.aws_lambda.Runtime.NODEJS_LATEST,
+      handler: "index.handler",
+      code: cdk.aws_lambda.Code.fromAsset(
+        path.join(__dirname, "services/readfile")
+      ),
+    });
   }
 }
