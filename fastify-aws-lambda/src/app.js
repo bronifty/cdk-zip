897f577085e3eae9ed56fb8a899ece180d6a33de
bronifty <bronifty@gmail.com>
2024-05-26 20:51:14 -0400

    this

---
+++ fastify-aws-lambda/src/app.js
@@ -0,0 +1,15 @@
+const fastify = require("fastify");
+
+const app = fastify();
+app.get("/", (request, reply) => reply.send({ hello: "world" }));
+
+if (require.main === module) {
+  // called directly i.e. "node app"
+  app.listen({ port: 3000 }, (err) => {
+    if (err) console.error(err);
+    console.log("server listening on 3000");
+  });
+} else {
+  // required as a module => executed on aws lambda
+  module.exports = app;
+}
